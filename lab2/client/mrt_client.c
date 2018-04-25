80d00c62b9f9ebb3993f258e6a03e8905d114987
Ross Reagan Guju <ross.r.guju.ug@dartmouth.edu>
2018-04-24 21:56:16 -0400

    I messed it up last minute- but turning it in now

--- lab2/client/mrt_client.c
+++ lab2/client/mrt_client.c
@@ -1,15 +1,20 @@
-/*
- * client/mrt_client.h: implementation of MRT client socket 
- * interfaces. 
- *
- * CS60, March 2018. 
- */
+/****
+Lab2 
+Ross Guju
+
+
+
+***/
+
+
 
 #include <stdlib.h>
+#include <strings.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <time.h>
+#include <errno.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <assert.h>
@@ -19,66 +24,441 @@
 /**************** global variables ****************/
 client_tcb_t *p_client_tcb_table[MAX_TRANSPORT_CONNECTIONS];
 int overlay_connection;
+pthread_mutex_t mutex;
+pthread_cond_t cond;
 
 /**************** functions ****************/
 
 // TODO - mrt client initialization
+
+
+void *segBuffer_timer(void* arg);
+void send_buff(client_tcb_t *client_TCB);
+void *buffer_timer(void* client);
+void append_buffer(client_tcb_t *client_TCB, segBuf_t *buffer);
+//int send_acknowledge_timer(seg_t *segment, client_tcb_t *client_TCB , unsigned int expect_state, unsigned int TimeOut);
+
+client_tcb_t *fetch_TCB( seg_t* newSegment);
+
 void mrt_client_init(int conn) {
+
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond, NULL);
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+    p_client_tcb_table[i] = NULL;
+  }
+  overlay_connection = conn;
+
+  pthread_t seghandler_thread;
+  pthread_create(&seghandler_thread, NULL, seghandler, (void *) 0);
+
 }
 
 // TODO - Create a client sock
 int mrt_client_sock(unsigned int client_port) {
-	return 1;
+  int i;
+  for (i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+
+    client_tcb_t *client_TCB;
+    if (p_client_tcb_table[i] == NULL) {
+      p_client_tcb_table[i] = (client_tcb_t*) malloc(sizeof(client_tcb_t));
+      client_TCB =  p_client_tcb_table[i];
+      client_TCB->bufMutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+      pthread_mutex_init(client_TCB->bufMutex, NULL);
+      client_TCB->state = CLIENT_CLOSED;
+      client_TCB->client_portNum = client_port;
+      client_TCB->next_seqNum = 0;
+      client_TCB->sendBufHead = NULL;
+      client_TCB->sendBufunSent = NULL;
+      client_TCB->sendBufTail = NULL;
+      client_TCB->unAck_segNum = 0;
+      return i;
+    }
+  }
+  return -1;
 }
 
-// TODO - Connect to the server
+/******* mrt_client_connect******/
+
 int mrt_client_connect(int sockfd, unsigned int server_port) {
-	return -1;
+  int l;
+  if (sockfd > MAX_TRANSPORT_CONNECTIONS || sockfd < 0) {
+    perror("mrt_client_connect failed\n");
+    return -1;
+  }
+
+// connect to the server by sending syn
+  client_tcb_t *client_TCB = p_client_tcb_table[sockfd];
+  seg_t *segment = (seg_t*) malloc(sizeof(seg_t));
+
+  client_TCB->svr_portNum = server_port;
+  segment->header.length = 0;
+  segment->header.type = SYN;
+  segment->header.src_port = client_TCB->client_portNum;
+  segment->header.dest_port = client_TCB->svr_portNum;
+  segment->header.seq_num = 0;
+  mnp_sendseg(overlay_connection, segment);
+  printf("For sending syn from client, the clients own port number is %d \n", client_TCB->client_portNum);
+
+
+
+  client_TCB->state = CLIENT_SYNSENT;
+// switch to synsent state
+  for (int i = 0; i < SYN_MAX_RETRY; i++) {
+
+    if (client_TCB->state == CLIENT_CONNECTED) {
+      printf("%s\n", "Client connected\n\n\n\n" );
+      mnp_sendseg(overlay_connection, segment);
+      return 1;
+    } else if (i == SYN_MAX_RETRY) {
+      mnp_sendseg(overlay_connection, segment);
+      return 1;
+    }
+    else {
+      mnp_sendseg(overlay_connection, segment);
+      continue;
+    }
+  }
+  if (client_TCB->state == CLIENT_CONNECTED) {
+    return 1;
+  }
+
+  return 1;
 }
+
+
+/******* mrt_client_send()******/
 
 // TODO - Send data to a mrt server
-int mrt_client_send(int sockfd, void *data, unsigned int length) {
-	return -1;
+int mrt_client_send(int sockfd, void *data, unsigned int length)
+{
+
+  segBuf_t *buffer;
+  client_tcb_t *client_TCB;
+  client_TCB = p_client_tcb_table[sockfd];
+
+
+  if (sockfd > MAX_TRANSPORT_CONNECTIONS || sockfd < 0) {
+    perror("mrt_client_send failed\n");
+    return -1;
+  }
+
+  while (p_client_tcb_table[sockfd]->state != CLIENT_CONNECTED ) {
+    sleep(1);
+  }
+
+  buffer = (segBuf_t *) malloc(sizeof(segBuf_t));
+
+//pthread_mutex_lock(client_TCB->bufMutex);
+  while (length > 0) {
+
+//intialize the buffer
+
+    buffer->seg.header.type = DATA;
+    buffer->seg.header.seq_num = client_TCB->next_seqNum;
+    buffer->seg.header.src_port = client_TCB->client_portNum;
+    buffer->seg.header.dest_port = client_TCB->svr_portNum;
+    buffer->sentTime = 0;
+    buffer->next = NULL;
+
+
+    if (length < MAX_SEG_LEN) {
+      memcpy(buffer->seg.data, data, length);
+      buffer->seg.header.length = length;
+      client_TCB->next_seqNum = client_TCB->next_seqNum + length;
+
+
+    }
+
+    if (client_TCB->sendBufHead == NULL) {
+      client_TCB->sendBufHead = buffer;
+      client_TCB->sendBufTail = buffer;
+      client_TCB->sendBufunSent = buffer;
+// } else {
+//      //client_TCB->sendBufunSent = buffer;
+//    client_TCB->sendBufTail->next = buffer;
+//    client_TCB->sendBufTail = buffer;
+
+
+    }
+//send buffer
+    if (client_TCB->sendBufunSent == NULL ) {
+
+      client_TCB->sendBufunSent = buffer;
+      client_TCB->sendBufTail->next = buffer;
+      client_TCB->sendBufTail = buffer;
+    }
+//pthread_mutex_unlock(client_TCB->bufMutex);
+    send_buff(client_TCB);
+    client_TCB->unAck_segNum = client_TCB->unAck_segNum + 1;
+
+  }
+//pthread_mutex_unlock(client_TCB->bufMutex);
+
+
+  return 1;
 }
+
+
 
 // TODO - Disconnect from the server
 int mrt_client_disconnect(int sockfd) {
-	return 1;
+
+  seg_t segment;
+  segBuf_t *segBuffer;
+  client_tcb_t *client_TCB = p_client_tcb_table[sockfd];
+
+
+  segment.header.seq_num = client_TCB->next_seqNum;
+  segment.header.type = FIN;
+  segment.header.length = 0;
+  segment.header.src_port = client_TCB->client_portNum;
+  segment.header.dest_port = client_TCB->svr_portNum;
+  client_TCB->state = CLIENT_FINWAIT;
+//send_acknowledge_timer(&segment, client_TCB, CLIENT_CLOSED, FINSEG_TIMEOUT_NS);
+
+//pthread_mutex_lock(client_TCB->bufMutex);
+  while ((segBuffer = client_TCB->sendBufHead) != NULL) {
+
+    client_TCB->sendBufHead = client_TCB->sendBufHead->next;
+    free(segBuffer);
+    segBuffer = client_TCB->sendBufHead;
+  }
+  client_TCB->state = CLIENT_CLOSED;
+  client_TCB->unAck_segNum = 0;
+  client_TCB->sendBufHead = NULL;
+  client_TCB->sendBufTail = NULL;
+  client_TCB->sendBufunSent = NULL;
+  //pthread_mutex_unlock(client_TCB->bufMutex);
+
+
+  return 1;
 }
 
-// TODO - Close client
+/**** mrt_client_close()******/
+
 int mrt_client_close(int sockfd) {
-	return 1;
+
+  client_tcb_t *client_TCB = p_client_tcb_table[sockfd];
+
+  if (client_TCB->state == CLIENT_CLOSED) {
+    // clean up
+    pthread_mutex_destroy(client_TCB->bufMutex);
+    free(client_TCB->bufMutex);
+    free(client_TCB);
+    client_TCB = NULL;
+    return 1;
+
+  }
+
+  return -1;
 }
 
 
-// TODO - Thread handles incoming segment
-/**
-* @example
-*
-* while(1)
-1) Get segment using function mnp_recvseg(), kill thread if something
- went wrong
-2) Find tcb to handle this segment by looking up tcbs using the dest
- port in the segment header
-3) switch(tcb state)
-  case CLOSED: break;  
-  case SYNSENT:
-    if(seg.header.type==SYNACK && tcb->svrport==seg.header.srcport)
-      set tcb state to CONNECTED
-    break;
-  case CONNECTED: 
-    if segBuf.type == DATAACK && tcb.svrport==segBuf.srcport
-      if segBuf.ack_num >= tcb.sendBufHead.seq_num
-        update send buf with ack_num
-        send new segments in send buffer
-    break;
-  case FINWAIT:
-    if(seg.header.type==FINACK && tcb->svrport==seg.header.srcport)
-      set tcb state to CLOSED
+void *seghandler(void *arg)
+{
+  seg_t segment;    //Received segment
+  seg_t segment_Received;    //Sending segment
+  client_tcb_t *client_TCB;
+  segBuf_t *buf;
+// while we are sill getting messages
+  while (mnp_recvseg(overlay_connection, &segment) != -1) {
+
+    client_TCB = fetch_TCB(&segment);
+
+    if (client_TCB == NULL) {
+
+      continue;
+    }
+    switch (client_TCB->state) {
+    // current state is closed
+    case CLIENT_CLOSED:
+      printf("Handling for closed\n");
       break;
-*
-*/
-void *seghandler(void *arg) {
+
+    // recieved syn and now send
+    case CLIENT_SYNSENT:
+      printf("Handling for synsent\n");
+      if (segment.header.type == SYNACK && client_TCB->svr_portNum == segment.header.src_port) {
+
+        client_TCB->state = CLIENT_CONNECTED;
+      }
+      break;
+
+    case CLIENT_CONNECTED:
+
+      pthread_mutex_lock(client_TCB->bufMutex);
+
+
+      if (segment.header.type == DATAACK && client_TCB->svr_portNum == segment.header.src_port) {
+
+// send the buffer
+        if (segment.header.seq_num > client_TCB->sendBufTail->seg.header.seq_num) {
+          segBuf_t *segBufp;
+          segBufp = client_TCB->sendBufHead;
+          while (segBufp && segBufp->seg.header.seq_num < segment.header.seq_num ) {
+            client_TCB->sendBufHead = segBufp->next;
+
+            segBuf_t* temp = segBufp;
+            segBufp = segBufp->next;
+            free(temp);
+            client_TCB->unAck_segNum--;
+          }
+          send_buff(client_TCB);
+        }
+      }
+
+      pthread_mutex_unlock(client_TCB->bufMutex);
+      break;
+    case CLIENT_FINWAIT:
+      if (segment.header.type == FINACK && client_TCB->svr_portNum == segment.header.src_port) {
+        client_TCB->state = CLIENT_CLOSED;
+        break;
+      }
+    }
+
+  }
+  return NULL;
+}
+
+/***** buffer_timer*****/
+
+void *buffer_timer(void* client)
+{
+
+  client_tcb_t *client_TCB;
+  client_TCB = (client_tcb_t*)client_TCB;
+  segBuf_t *buf;
+  int i;
+
+  struct timespec req;
+
+
+// go through and get the time sent
+  for (;;) {
+
+    req.tv_sec = 0;
+    req.tv_nsec = 10000000;
+    nanosleep(&req, NULL);
+
+    if (client_TCB->sendBufHead == NULL || client_TCB->sendBufHead == client_TCB->sendBufunSent)
+    {
+      break;
+    }
+
+    i = time(NULL) - client_TCB->sendBufHead->sentTime;
+    if (i > DATA_TIMEOUT)
+    {
+
+
+      buf = client_TCB->sendBufHead;
+      while (buf != client_TCB->sendBufunSent)
+      {
+        mnp_sendseg(overlay_connection, &buf->seg);
+        buf->sentTime = time(NULL);
+        buf = buf->next;
+      }
+    }
+  }
+  return NULL;
+}
+
+
+
+/******* send_acknowloedgement_timer() *******/
+
+
+int send_acknowledge_timer(seg_t *segment, client_tcb_t *client_TCB , unsigned int expect_state, unsigned int TimeOut)
+{
+  int max_syn_reached;
+  struct timespec timer;
+
+  int i = 1;
+  for (i = 1; i < SYN_MAX_RETRY; i++) {
+    {
+      mnp_sendseg(overlay_connection, segment);
+
+      pthread_mutex_lock(&mutex);
+
+      clock_gettime(CLOCK_MONOTONIC, &timer);
+
+      timer.tv_nsec = (timer.tv_nsec + TimeOut) % SYNSEG_TIMEOUT_NS;
+
+      max_syn_reached = pthread_cond_timedwait(&cond, &mutex, &timer);
+
+
+      pthread_mutex_unlock(&mutex);
+
+      if  (max_syn_reached  == ETIMEDOUT) {
+        continue;
+      }
+
+      if (client_TCB->state == expect_state) {
+
+        return 1;
+
+      }
+    }
+  }
+
+  return -1;
+}
+/**** fecthed_TCB****/
+client_tcb_t *fetch_TCB( seg_t* newSegment)
+{
+
+  client_tcb_t *client_TCB;
+
+  int seg_portnum = newSegment->header.dest_port;
+// get the data structure acdording to the socket file descriptor
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+    client_TCB = p_client_tcb_table[i];
+    if (client_TCB == NULL) {
+      continue;
+    }
+
+    if (client_TCB->client_portNum == seg_portnum) {
+      return client_TCB;
+    }
+
+  }
+  return NULL;
 
 }
+
+/********* send_buffer()********/
+void send_buff(client_tcb_t *client_TCB)
+{
+  pthread_t ThreadID;
+
+  int i;
+
+  pthread_mutex_lock(client_TCB->bufMutex);
+  for ((i = GBN_WINDOW - client_TCB->unAck_segNum); i > 0; i--) {
+// while (client_TCB->sendBufunSent != NULL ){
+
+    if (client_TCB->sendBufunSent != NULL) {
+      mnp_sendseg(overlay_connection, &client_TCB->sendBufunSent->seg);
+//sleep(1);
+      client_TCB->sendBufunSent->sentTime = time(NULL);
+//client_TCB->unAck_segNum = (client_TCB->unAck_segNum  + 1);
+
+      if (client_TCB->sendBufunSent == client_TCB->sendBufHead) {
+
+        if (pthread_create(&ThreadID, NULL, buffer_timer, (void*)client_TCB) != 0) {
+
+        }
+
+      }
+      client_TCB->sendBufunSent = client_TCB->sendBufunSent->next;
+
+    }
+
+    if ( client_TCB->sendBufunSent == NULL) {
+      break;
+    }
+  }
+  pthread_mutex_unlock(client_TCB->bufMutex);
+  return;
+}
+
--- lab2/server/mrt_server.c
+++ lab2/server/mrt_server.c
@@ -1,8 +1,11 @@
 /*
- * server/mrt_server.c: implementation of MRT server socket 
- * interfaces. 
+ * server/mrt_server.c: implementation of MRT server socket
+ * interfaces.
+ * Ross Guju
+ * Lab2
  *
- * CS60, March 2018. 
+ *
+ * CS60, March 2018.
  */
 
 #include <stdlib.h>
@@ -12,47 +15,179 @@
 #include <time.h>
 #include <pthread.h>
 #include <assert.h>
+#include <wchar.h>
+#include <string.h>
 #include <memory.h>
 #include "mrt_server.h"
 
 /**************** global variables ****************/
 server_tcb_t *p_server_tcb_table[MAX_TRANSPORT_CONNECTIONS]; // TCB table
 int overlay_connection; // overlay_connection
-
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+void *closewait_handle( void *args );
+void close_wait_timeout(server_tcb_t *server_TCB);
+void *timer_handler(void *args);
+server_tcb_t *fetch_TCB(unsigned int svr_portNum);
 
 /**************** functions ****************/
 
 // TODO - mrt server initialization
 void mrt_server_init(int conn) {
-	/* initialize the global variables */
-	for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
-		p_server_tcb_table[i] = NULL;
-	}
-	overlay_connection = conn;
+  /* initialize the global variables */
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+    p_server_tcb_table[i] = NULL;
+  }
+  overlay_connection = conn;
 
-	/* Create a thread for handling connections */
-	pthread_t seghandler_thread;
-	pthread_create(&seghandler_thread, NULL, seghandler, (void *) 0);
+  /* Create a thread for handling connections */
+  pthread_t seghandler_thread;
+  pthread_create(&seghandler_thread, NULL, seghandler, (void *) 0);
 }
 
 // TODO - Create a server sock and setting the recv buffer
+//
+/******* mrt_server_sock()******/
+
 int mrt_server_sock(unsigned int port) {
-	return 0;
+
+
+  server_tcb_t *server_TCB;
+
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+
+    if (p_server_tcb_table[i] == NULL ) {
+
+// initialize variables
+      server_TCB = p_server_tcb_table[i] = malloc(sizeof(server_tcb_t));
+
+      server_TCB->state = SVR_CLOSED;
+      server_TCB->svr_portNum = port;
+      server_TCB->recvBuf = malloc(RECEIVE_BUF_SIZE);
+      server_TCB->bufMutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+      server_TCB->usedBufLen = 0;
+      pthread_mutex_init(server_TCB->bufMutex, NULL);
+
+      return i;
+
+    }
+
+  }
+
+  return -1;
 }
 
-// TODO - Accept overlay_connection from mrt client
+/******* mrt_server_accept *****/
+
 int mrt_server_accept(int sockfd) {
-	return -1;
+//  struct timeval timer;
+  server_tcb_t *server_TCB;
+  server_TCB = p_server_tcb_table[sockfd];
+//timer.tv_sec = 0;
+  //timer.tv_usec = ACCEPT_POLLING_INTERVAL / 1000;
+
+  if (server_TCB->state == SVR_CLOSED) {
+    server_TCB->state = SVR_LISTENING;
+
+
+
+    printf("%s\n", "Waiting to accept\n" );
+    //sleep(1);
+    for (;;) {
+// if in connected state return 1
+      if (server_TCB->state == SVR_CONNECTED) {
+        printf("accepted state & connnected\n");
+        return 1;
+      }
+    }
+  }
+  sleep(1);
+  return -1;
 }
 
-// TODO - Receive data from a mrt client
+
 int mrt_server_recv(int sockfd, void *buf, unsigned int length) {
-	return 1;
+
+  int i, l;
+
+  if (sockfd < 0 || sockfd > MAX_TRANSPORT_CONNECTIONS) {
+    printf("something went wrong.");
+    return -1;
+  }
+
+  server_tcb_t *server_TCB;
+
+  server_TCB = p_server_tcb_table[sockfd];
+// initialize input buffer
+  char* data;
+  data = (char*) buf;
+
+  if (length > server_TCB->usedBufLen) {
+    sleep(RECVBUF_POLLING_INTERVAL);
+  }
+
+
+
+  pthread_mutex_lock(server_TCB->bufMutex);
+
+// get the buffer.
+
+  for (i = 0; i < length; i++) {
+    data[i] = server_TCB->recvBuf[i];
+  }
+
+  for (i = 0; i < server_TCB->usedBufLen; i++) {
+    server_TCB->recvBuf[l] = server_TCB->recvBuf[i];
+    l++;
+  }
+
+  server_TCB->usedBufLen -= length;
+  return 1;
+  pthread_mutex_unlock(server_TCB->bufMutex);
+
 }
 
-// TODO - Close the mrt server and Free all the space
+/*****mrt_server_close*****/
+
 int mrt_server_close(int sockfd) {
-	return 1;
+  if (sockfd < 0 || sockfd > MAX_TRANSPORT_CONNECTIONS) {
+    printf("something went wrong.");
+    return -1;
+  }
+  server_tcb_t *server_TCB = p_server_tcb_table[sockfd];
+
+  if (server_TCB != NULL) {
+    printf("mrt_server_close(int sockfd)\n");
+    switch (server_TCB->state) {
+    case (SVR_LISTENING):
+//close in the listening state
+      printf("current state is not SVR_CLOSED\n");
+      printf("Changing to SRV_CLOSED\n");
+      break;
+
+    case (SVR_CONNECTED):
+//close in the sever connect state
+      printf("sockfd is in connecged state\n");
+      printf("Changing to SRV_CLOSED\n");
+      break;
+
+    default:
+      printf("Changing current state to SRV_CLOSED\n");
+      break;
+    }
+  }
+// clean up
+  server_TCB->state = SVR_CLOSED;
+  pthread_mutex_destroy(server_TCB->bufMutex);
+  free(server_TCB->recvBuf);
+  free(server_TCB->bufMutex);
+  free(server_TCB);
+  server_TCB->recvBuf = NULL;
+  server_TCB->bufMutex = NULL;
+  server_TCB = NULL;
+
+
+  return 1;
 }
 
 
@@ -87,12 +222,190 @@ int mrt_server_close(int sockfd) {
         spawn thread to handle CLOSEWAIT state transition (see closewait below)
       break;
 
-    case CLOSEWAIT: 
+    case CLOSEWAIT:
       if (seg.header.type==FIN && tcb.client_port == seg.header.srcport)
         send FINACK with fin_received call
       break;
  *
  */
+
+/****seghandler()*****/
+
 void *seghandler(void *arg) {
-  
+
+  seg_t segment;      //REceived segment
+  seg_t segment_send;      //Sendingsegment
+
+
+  server_tcb_t *server_TCB;
+  while (1) {
+    server_TCB = fetch_TCB(segment.header.dest_port);
+    mnp_recvseg(overlay_connection, &segment);
+
+    if (server_TCB == NULL) {
+      continue;
+    }
+
+    printf("recv string: %s from overlay \n", segment.data);
+
+    switch (server_TCB->state) {
+
+    case SVR_CLOSED:
+      break;
+      //case when state is listening
+    case SVR_LISTENING:
+
+      if (segment.header.type == SYN) {
+        memset(&segment_send, 0, sizeof(seg_t));
+
+        server_TCB->expect_seqNum = segment.header.seq_num;
+        segment_send.header.length = 0;
+        server_TCB->client_portNum = segment.header.src_port;
+        server_TCB->state = SVR_CONNECTED;
+        segment_send.header.dest_port = server_TCB->client_portNum;
+        segment_send.header.src_port = server_TCB->svr_portNum;
+        segment_send.header.type = SYNACK;
+
+        mnp_sendseg(overlay_connection, &segment_send);
+
+      }
+      break;
+      //case when stat is connected
+    case SVR_CONNECTED:
+      if (segment.header.type == SYN && server_TCB->client_portNum == segment.header.src_port) {
+        memset(&segment_send, 0, sizeof(seg_t));
+
+        segment_send.header.type = SYNACK;
+        segment_send.header.seq_num = server_TCB->expect_seqNum;
+        segment_send.header.length = 0;
+        segment.header.src_port = server_TCB->svr_portNum;
+        segment.header.dest_port = server_TCB->client_portNum;
+
+
+        mnp_sendseg(overlay_connection, &segment_send);
+      }
+      // if server recieves data
+      else if (segment.header.type == DATA && server_TCB->client_portNum == segment.header.src_port) {
+
+        // pthread_mutex_lock(server_TCB->bufMutex);
+
+
+
+        if (segment.header.seq_num == server_TCB->expect_seqNum) {
+          if ((segment.header.length + server_TCB->usedBufLen) < RECEIVE_BUF_SIZE) {
+
+            // for (int k = 0; k < segment.header.length; k++) {
+            //   printf("%c", segment.data[k]  );
+            // }
+
+           
+
+            memcpy(&server_TCB->recvBuf[server_TCB->usedBufLen], segment.data, segment.header.length);
+            server_TCB->usedBufLen += segment.header.length;
+            server_TCB->expect_seqNum = segment.header.seq_num + segment.header.length;
+          }
+        } else {
+
+
+          memset(&segment_send, 0, sizeof(seg_t));
+          segment_send.header.type = DATAACK;
+          segment_send.header.src_port = server_TCB->svr_portNum;
+          segment_send.header.dest_port = server_TCB->client_portNum;
+          segment_send.header.ack_num = server_TCB->expect_seqNum;
+          //pthread_mutex_lock(server_TCB->bufMutex);
+          //memmove((server_TCB->recvBuf + server_TCB->usedBufLen), segment.data, segment.header.length);
+
+          segment_send.header.length = 0;
+          //server_TCB->usedBufLen += segment.header.length;
+          // server_TCB->expect_seqNum += segment.header.length;
+
+
+
+          mnp_sendseg(overlay_connection, &segment_send);
+
+        }
+      }
+      else if (segment.header.type == FIN && server_TCB->client_portNum == segment.header.src_port) {
+        segment_send.header.type = FINACK;
+        server_TCB->state = SVR_CLOSEWAIT;
+        //server_TCB->state = SVR_CLOSEWAIT;
+        segment_send.header.ack_num = server_TCB->expect_seqNum;
+        segment_send.header.length = 0;
+        segment_send.header.type = FINACK;
+
+        segment_send.header.src_port = server_TCB->svr_portNum;
+        segment_send.header.dest_port = server_TCB->client_portNum;
+        // mnp_recvseg(overlay_connection, &segment);
+        mnp_sendseg(overlay_connection, &segment_send);
+        close_wait_timeout(server_TCB);
+
+      }
+      break;
+
+    case SVR_CLOSEWAIT:
+      if (segment.header.type == FIN && server_TCB->client_portNum == segment.header.src_port) {
+        segment_send.header.type = FINACK;
+        segment_send.header.ack_num = server_TCB->expect_seqNum;
+        segment_send.header.length = 0;
+        segment_send.header.src_port = server_TCB->svr_portNum;
+        segment_send.header.dest_port = server_TCB->client_portNum;
+        mnp_sendseg(overlay_connection, &segment_send);
+      }
+      break;
+
+    default:
+      break;
+    }
+  }
+  return NULL;
 }
+
+/**** fetch_TCB()*****/
+
+
+server_tcb_t *fetch_TCB(unsigned int svr_portNum)
+{
+
+  server_tcb_t *server_TCB;
+// get the associated sever data structure
+  for (int i = 0; i < MAX_TRANSPORT_CONNECTIONS; i++) {
+    server_TCB = p_server_tcb_table[i];
+    if (server_TCB != NULL && server_TCB->svr_portNum == svr_portNum) {
+
+      return server_TCB;
+    }
+    else if (server_TCB == NULL) {
+      continue;
+    }
+  }
+  return NULL;
+
+}
+void *timer_handler(void *args)
+{
+  // handles cloasing time
+  sleep(CLOSEWAIT_TIME);
+  server_tcb_t *server_TCB;
+  server_TCB = (server_tcb_t *)args;
+
+  pthread_mutex_lock(server_TCB->bufMutex);
+  server_TCB->state = SVR_CLOSED;
+  server_TCB->usedBufLen = 0;
+  pthread_mutex_unlock(server_TCB->bufMutex);
+
+  return NULL;
+}
+
+void close_wait_timeout(server_tcb_t *server_TCB)
+{
+  pthread_t pthreadID;
+
+//time out
+  if (pthread_create(&pthreadID, NULL, timer_handler, (void *)server_TCB) != 0) {
+
+    printf("close_wait_timeout create pthread failed\n");
+    exit(0);
+  }
+}
+
+
